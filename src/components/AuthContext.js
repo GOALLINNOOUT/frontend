55b58e798401aea5bef8f8b24d356f1cad11734c
ADELA009 <PHILIPADELA12@outlook.com>
2025-07-12 15:56:12 +0100

    ggg

--- src/components/AuthContext.js
+++ src/components/AuthContext.js
@@ -1,3 +1,6 @@
+
 import { createContext } from 'react';
+import { AuthProvider } from './AuthContext.jsx';
 
 export const AuthContext = createContext({ user: null, setUser: () => {} });
+export { AuthProvider };
