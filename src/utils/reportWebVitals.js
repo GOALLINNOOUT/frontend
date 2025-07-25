4d114b4853d89c44140d558cd93015bc0647d0a9
ADELA009 <PHILIPADELA12@outlook.com>
2025-07-25 02:05:14 +0100

    logs

--- src/utils/reportWebVitals.js
+++ src/utils/reportWebVitals.js
@@ -3,20 +3,25 @@ import * as webVitals from 'web-vitals';
 import * as api from './api';
 
 function sendToAnalytics(metric) {
-  // Optionally add more context (page, session, user, etc.)
+  // Debug: log every metric sent
+  console.log('[WebVitals] Sending metric:', metric);
   api.post('/v1/analytics/web-vitals', {
     ...metric,
     page: window.location.pathname,
     url: window.location.href,
     // Add sessionId/userId if available
+  }).then(res => {
+    console.log('[WebVitals] API response:', res);
+  }).catch(err => {
+    console.error('[WebVitals] API error:', err);
   });
 }
 
 export function reportWebVitals() {
-  if (webVitals.getCLS) webVitals.getCLS(sendToAnalytics);
-  if (webVitals.getFID) webVitals.getFID(sendToAnalytics);
-  if (webVitals.getLCP) webVitals.getLCP(sendToAnalytics);
-  if (webVitals.getFCP) webVitals.getFCP(sendToAnalytics);
-  if (webVitals.getTTFB) webVitals.getTTFB(sendToAnalytics);
-  if (webVitals.getINP) webVitals.getINP(sendToAnalytics);
+  if (webVitals.getCLS) webVitals.getCLS(m => { console.log('[WebVitals] CLS:', m); sendToAnalytics(m); });
+  if (webVitals.getFID) webVitals.getFID(m => { console.log('[WebVitals] FID:', m); sendToAnalytics(m); });
+  if (webVitals.getLCP) webVitals.getLCP(m => { console.log('[WebVitals] LCP:', m); sendToAnalytics(m); });
+  if (webVitals.getFCP) webVitals.getFCP(m => { console.log('[WebVitals] FCP:', m); sendToAnalytics(m); });
+  if (webVitals.getTTFB) webVitals.getTTFB(m => { console.log('[WebVitals] TTFB:', m); sendToAnalytics(m); });
+  if (webVitals.getINP) webVitals.getINP(m => { console.log('[WebVitals] INP:', m); sendToAnalytics(m); });
 }
