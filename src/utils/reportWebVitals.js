57fbc70be1a2b5846f2fa442aa3dfe28789f052c
ADELA009 <PHILIPADELA12@outlook.com>
2025-07-25 02:45:29 +0100

    up

--- src/utils/reportWebVitals.js
+++ src/utils/reportWebVitals.js
@@ -1,7 +1,7 @@
 
 console.log('[WebVitals] reportWebVitals.js loaded');
 // Utility to report web-vitals from the browser to the backend
-import { getCLS, getFID, getLCP, getFCP, getTTFB, getINP } from 'web-vitals/attribution';
+import * as webVitals from 'web-vitals/attribution';
 import * as api from './api';
 
 function sendToAnalytics(metric) {
@@ -21,10 +21,10 @@ function sendToAnalytics(metric) {
 
 export function reportWebVitals() {
   console.log('[WebVitals] reportWebVitals() called');
-  getCLS(m => { console.log('[WebVitals] CLS:', m); sendToAnalytics(m); });
-  getFID(m => { console.log('[WebVitals] FID:', m); sendToAnalytics(m); });
-  getLCP(m => { console.log('[WebVitals] LCP:', m); sendToAnalytics(m); });
-  getFCP(m => { console.log('[WebVitals] FCP:', m); sendToAnalytics(m); });
-  getTTFB(m => { console.log('[WebVitals] TTFB:', m); sendToAnalytics(m); });
-  if (typeof getINP === 'function') getINP(m => { console.log('[WebVitals] INP:', m); sendToAnalytics(m); });
+  if (typeof webVitals.getCLS === 'function') webVitals.getCLS(m => { console.log('[WebVitals] CLS:', m); sendToAnalytics(m); });
+  if (typeof webVitals.getFID === 'function') webVitals.getFID(m => { console.log('[WebVitals] FID:', m); sendToAnalytics(m); });
+  if (typeof webVitals.getLCP === 'function') webVitals.getLCP(m => { console.log('[WebVitals] LCP:', m); sendToAnalytics(m); });
+  if (typeof webVitals.getFCP === 'function') webVitals.getFCP(m => { console.log('[WebVitals] FCP:', m); sendToAnalytics(m); });
+  if (typeof webVitals.getTTFB === 'function') webVitals.getTTFB(m => { console.log('[WebVitals] TTFB:', m); sendToAnalytics(m); });
+  if (typeof webVitals.getINP === 'function') webVitals.getINP(m => { console.log('[WebVitals] INP:', m); sendToAnalytics(m); });
 }
