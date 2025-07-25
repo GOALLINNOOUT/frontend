16301ec734c8152d997e4d057dde7b7dd2af33d6
ADELA009 <PHILIPADELA12@outlook.com>
2025-07-25 11:06:02 +0100

    update

--- src/utils/reportWebVitals.js
+++ src/utils/reportWebVitals.js
@@ -1,17 +1,18 @@
-
 console.log('[WebVitals] reportWebVitals.js loaded');
-// Utility to report web-vitals from the browser to the backend
+
+// Debug the import first
 import * as webVitals from 'web-vitals/attribution';
 import * as api from './api';
 
+console.log('[WebVitals] webVitals object:', webVitals);
+console.log('[WebVitals] Available functions:', Object.keys(webVitals));
+
 function sendToAnalytics(metric) {
-  // Debug: log every metric sent
   console.log('[WebVitals] Sending metric:', metric);
   api.post('/v1/analytics/web-vitals', {
     ...metric,
     page: window.location.pathname,
     url: window.location.href,
-    // Add sessionId/userId if available
   }).then(res => {
     console.log('[WebVitals] API response:', res);
   }).catch(err => {
@@ -21,10 +22,89 @@ function sendToAnalytics(metric) {
 
 export function reportWebVitals() {
   console.log('[WebVitals] reportWebVitals() called');
-  if (typeof webVitals.getCLS === 'function') webVitals.getCLS(m => { console.log('[WebVitals] CLS:', m); sendToAnalytics(m); });
-  if (typeof webVitals.getFID === 'function') webVitals.getFID(m => { console.log('[WebVitals] FID:', m); sendToAnalytics(m); });
-  if (typeof webVitals.getLCP === 'function') webVitals.getLCP(m => { console.log('[WebVitals] LCP:', m); sendToAnalytics(m); });
-  if (typeof webVitals.getFCP === 'function') webVitals.getFCP(m => { console.log('[WebVitals] FCP:', m); sendToAnalytics(m); });
-  if (typeof webVitals.getTTFB === 'function') webVitals.getTTFB(m => { console.log('[WebVitals] TTFB:', m); sendToAnalytics(m); });
-  if (typeof webVitals.getINP === 'function') webVitals.getINP(m => { console.log('[WebVitals] INP:', m); sendToAnalytics(m); });
+  console.log('[WebVitals] webVitals functions available:');
+  console.log('- getCLS:', typeof webVitals.getCLS);
+  console.log('- getFID:', typeof webVitals.getFID);
+  console.log('- getLCP:', typeof webVitals.getLCP);
+  console.log('- getFCP:', typeof webVitals.getFCP);
+  console.log('- getTTFB:', typeof webVitals.getTTFB);
+  console.log('- getINP:', typeof webVitals.getINP);
+
+  // Try calling them with more error handling
+  try {
+    if (typeof webVitals.getCLS === 'function') {
+      console.log('[WebVitals] Calling getCLS...');
+      webVitals.getCLS(m => { 
+        console.log('[WebVitals] CLS callback fired:', m); 
+        sendToAnalytics(m); 
+      });
+    } else {
+      console.log('[WebVitals] getCLS not available');
+    }
+
+    if (typeof webVitals.getFID === 'function') {
+      console.log('[WebVitals] Calling getFID...');
+      webVitals.getFID(m => { 
+        console.log('[WebVitals] FID callback fired:', m); 
+        sendToAnalytics(m); 
+      });
+    } else {
+      console.log('[WebVitals] getFID not available');
+    }
+
+    if (typeof webVitals.getLCP === 'function') {
+      console.log('[WebVitals] Calling getLCP...');
+      webVitals.getLCP(m => { 
+        console.log('[WebVitals] LCP callback fired:', m); 
+        sendToAnalytics(m); 
+      });
+    } else {
+      console.log('[WebVitals] getLCP not available');
+    }
+
+    if (typeof webVitals.getFCP === 'function') {
+      console.log('[WebVitals] Calling getFCP...');
+      webVitals.getFCP(m => { 
+        console.log('[WebVitals] FCP callback fired:', m); 
+        sendToAnalytics(m); 
+      });
+    } else {
+      console.log('[WebVitals] getFCP not available');
+    }
+
+    if (typeof webVitals.getTTFB === 'function') {
+      console.log('[WebVitals] Calling getTTFB...');
+      webVitals.getTTFB(m => { 
+        console.log('[WebVitals] TTFB callback fired:', m); 
+        sendToAnalytics(m); 
+      });
+    } else {
+      console.log('[WebVitals] getTTFB not available');
+    }
+
+    if (typeof webVitals.getINP === 'function') {
+      console.log('[WebVitals] Calling getINP...');
+      webVitals.getINP(m => { 
+        console.log('[WebVitals] INP callback fired:', m); 
+        sendToAnalytics(m); 
+      });
+    } else {
+      console.log('[WebVitals] getINP not available');
+    }
+  } catch (error) {
+    console.error('[WebVitals] Error calling web vitals functions:', error);
+  }
 }
+
+// Also try the alternative import path
+setTimeout(() => {
+  console.log('[WebVitals] Trying alternative import after delay...');
+  try {
+    import('web-vitals').then(webVitalsDefault => {
+      console.log('[WebVitals] Default import:', webVitalsDefault);
+      console.log('[WebVitals] Default import keys:', Object.keys(webVitalsDefault));
+    });
+  } catch (e) {
+    console.error('[WebVitals] Alternative import failed:', e);
+  }
+}, 1000);
