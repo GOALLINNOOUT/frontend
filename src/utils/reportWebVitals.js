7276526630ace1e223629408c366f9fc695528a2
ADELA009 <PHILIPADELA12@outlook.com>
2025-07-25 02:39:52 +0100

    lola

--- src/utils/reportWebVitals.js
+++ src/utils/reportWebVitals.js
@@ -1,7 +1,7 @@
 
 console.log('[WebVitals] reportWebVitals.js loaded');
 // Utility to report web-vitals from the browser to the backend
-import * as webVitals from 'web-vitals';
+import { onCLS, onFID, onLCP, onFCP, onTTFB, onINP } from 'web-vitals';
 import * as api from './api';
 
 function sendToAnalytics(metric) {
@@ -21,10 +21,10 @@ function sendToAnalytics(metric) {
 
 export function reportWebVitals() {
   console.log('[WebVitals] reportWebVitals() called');
-  if (typeof webVitals.getCLS === 'function') webVitals.getCLS(m => { console.log('[WebVitals] CLS:', m); sendToAnalytics(m); });
-  if (typeof webVitals.getFID === 'function') webVitals.getFID(m => { console.log('[WebVitals] FID:', m); sendToAnalytics(m); });
-  if (typeof webVitals.getLCP === 'function') webVitals.getLCP(m => { console.log('[WebVitals] LCP:', m); sendToAnalytics(m); });
-  if (typeof webVitals.getFCP === 'function') webVitals.getFCP(m => { console.log('[WebVitals] FCP:', m); sendToAnalytics(m); });
-  if (typeof webVitals.getTTFB === 'function') webVitals.getTTFB(m => { console.log('[WebVitals] TTFB:', m); sendToAnalytics(m); });
-  if (typeof webVitals.getINP === 'function') webVitals.getINP(m => { console.log('[WebVitals] INP:', m); sendToAnalytics(m); });
+  onCLS(m => { console.log('[WebVitals] CLS:', m); sendToAnalytics(m); });
+  onFID(m => { console.log('[WebVitals] FID:', m); sendToAnalytics(m); });
+  onLCP(m => { console.log('[WebVitals] LCP:', m); sendToAnalytics(m); });
+  onFCP(m => { console.log('[WebVitals] FCP:', m); sendToAnalytics(m); });
+  onTTFB(m => { console.log('[WebVitals] TTFB:', m); sendToAnalytics(m); });
+  if (typeof onINP === 'function') onINP(m => { console.log('[WebVitals] INP:', m); sendToAnalytics(m); });
 }
